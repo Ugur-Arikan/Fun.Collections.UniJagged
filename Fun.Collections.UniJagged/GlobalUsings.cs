21a08950389f67da5f9a1a5f6e37338dbba51944
Ugur-Arikan <uurfun@gmail.com>
2023-01-21 23:20:51 +0100

    sandbox

--- Fun.Collections.UniJagged/GlobalUsings.cs
+++ Fun.Collections.UniJagged/GlobalUsings.cs
@@ -1,3 +1,18 @@
 ﻿global using OptRes;
 global using static OptRes.Extensions;
 global using static Fun.Arrays.Shorthands;
+using System.Runtime.CompilerServices;
+
+namespace Fun.Collections
+{
+    /// <summary>
+    /// Jagged array representations, wrapping different underlying data types by a functional approach in order to achieve a unified collection type.
+    /// </summary>
+    [CompilerGeneratedAttribute()]
+    class NamespaceDoc { }
+
+    internal static class Exc
+    {
+        internal static NotImplementedException MustNotReach = new("Must not have reached here!");
+    }
+}
